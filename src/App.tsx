1c3d21ec9a27a691ebb21f81f96efb495526cc83
AlwanZ <alwanzharif42@gmail.com>
2024-05-02 09:34:11 +0700

    tambah link vercell @app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react'
 import DataList from './components/data-list'
 
+//Sudah Deploy ke vercell link: https://react-crud-alwan.vercel.app/
+
 const App: FC = () => {
   return ( <div><DataList /></div>
 )
